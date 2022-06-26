2496347f0632473e8c4ed96044d51e9ea352bceb
OmniPerson <ginthetonic@gmail.com>
2022-06-26 15:04:32 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Game
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.Write("What is your name? ");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}");
+        }
+    }
+}
+
